dbebc7379d1bc60e240ae5ff1c9b2e54aea986d3
dazzlerabhi30800 <abhisingh30800@gmail.com>
2024-02-28 09:45:25 +0530

    added env file

--- src/appWrite.tsx
+++ src/appWrite.tsx
@@ -1,14 +1,14 @@
 import { Account, Client, Databases } from "appwrite";
 
-export const Database_id = "65d2e1ba98dc506d4c9e";
-export const Collection_id = "65d2e1c1ceefca180096";
-// export const Document_id = "65d2e2504a1157eb6070";
+export const Database_id = import.meta.env.VITE_DATABASE_ID;
+export const Collection_id = import.meta.env.VITE_COLLECTION_ID;
 
 const client = new Client();
 export const account = new Account(client);
 
-client .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject("65d2e06571f6b3feabd6");
+client
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject(import.meta.env.VITE_PROJECT_ID);
 
 export const databases = new Databases(client);
 
